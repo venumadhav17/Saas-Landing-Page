3cb8646ba5b47e06b53399287d064491464e91c7
venumadhav17 <kalyanamvenumadhav@gmail.com>
2025-03-23 14:01:06 +0530

    added Integrations section, grid, slice, reverse(), typescript, created IntegrationColumn.tsx file

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -6,7 +6,7 @@ export default function Tag(props: HTMLAttributes<HTMLDivElement>) {
     return (
         <div
             className={twMerge(
-                "inline-flex border border-lime-400 gap-2 text-lime-400 px-3 py-1 rounded-full uppercase items-center",
+                "inline-flex border border-lime-400 gap-1.5 text-lime-400 px-3 py-1 rounded-full uppercase items-center",
                 className
             )}
             {...otherProps}
--- src/sections/Integrations.tsx
+++ src/sections/Integrations.tsx
@@ -1,12 +1,98 @@
+import Tag from "@/components/Tag";
+import figmaIcon from "@/assets/images/figma-logo.svg";
+import notionIcon from "@/assets/images/notion-logo.svg";
+import slackIcon from "@/assets/images/slack-logo.svg";
+import relumeIcon from "@/assets/images/relume-logo.svg";
+import framerIcon from "@/assets/images/framer-logo.svg";
+import githubIcon from "@/assets/images/github-logo.svg";
+import Image from "next/image";
+import IntegrationColumn from "@/components/IntegrationColumn";
+
 const integrations = [
-    { name: "Figma", icon: "figma-logo.svg", description: "Figma is a collaborative interface design tool." },
-    { name: "Notion", icon: "notion-logo.svg", description: "Notion is an all-in-one workspace for notes and docs." },
-    { name: "Slack", icon: "slack-logo.svg", description: "Slack is a powerful team communication platform." },
-    { name: "Relume", icon: "relume-logo.svg", description: "Relume is a no-code website builder and design system." },
-    { name: "Framer", icon: "framer-logo.svg", description: "Framer is a professional website prototyping tool." },
-    { name: "GitHub", icon: "github-logo.svg", description: "GitHub is the leading platform for code collaboration." },
+    {
+        name: "Figma",
+        icon: figmaIcon,
+        description: "Figma is a collaborative interface design tool.",
+    },
+    {
+        name: "Notion",
+        icon: notionIcon,
+        description: "Notion is an all-in-one workspace for notes and docs.",
+    },
+    {
+        name: "Slack",
+        icon: slackIcon,
+        description: "Slack is a powerful team communication platform.",
+    },
+    {
+        name: "Relume",
+        icon: relumeIcon,
+        description: "Relume is a no-code website builder and design system.",
+    },
+    {
+        name: "Framer",
+        icon: framerIcon,
+        description: "Framer is a professional website prototyping tool.",
+    },
+    {
+        name: "GitHub",
+        icon: githubIcon,
+        description: "GitHub is the leading platform for code collaboration.",
+    },
 ];
 
+export type IntegrationsType = typeof integrations;
+
 export default function Integrations() {
-    return <div>Integrations</div>;
+    return (
+        <section className="py-24 overflow-hidden">
+            <div className="container">
+                <div className="grid lg:grid-cols-2 items-center lg:gap-16">
+                    <div>
+                        <Tag>Integrations</Tag>
+                        <h2 className="text-6xl font-medium mt-6">
+                            Plays well with{" "}
+                            <span className="text-lime-400">others</span>
+                        </h2>
+                        <p className="text-white/50 mt-4 text-lg">
+                            Layers seamlessly connects with your favorite tools,
+                            making it easy to plug into any workflow and
+                            collaborate across platforms.
+                        </p>
+                    </div>
+                    <div>
+                        <div className="h-[400px] lg:h-[800px] mt-8 lg:mt-0 overflow-hidden grid md:grid-cols-2 gap-4 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]">
+                            <IntegrationColumn integrations={integrations} />
+                            <IntegrationColumn
+                                integrations={integrations.slice().reverse()}
+                                className="hidden md:flex"
+                            />
+                            {/*<div className="flex flex-col gap-4 pb-4">
+                        {integrations.map((integration) => (
+                            <div
+                                key={integration.name}
+                                className="bg-neutral-900 border border-white/10 rounded-3xl p-6"
+                            >
+                                <div className="flex justify-center">
+                                    <Image
+                                        src={integration.icon}
+                                        alt={`${integration.name} icon`}
+                                        className="size-24"
+                                    />
+                                </div>
+                                <h3 className="text-3xl text-center mt-6">
+                                    {integration.name}
+                                </h3>
+                                <p className="text-center text-white/50 mt-2">
+                                    {integration.description}
+                                </p>
+                            </div>
+                        ))}
+                    </div>*/}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
